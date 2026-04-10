35fbab883b7e074d0cbf5c5eb4629b748da0e2f0
JoseNB1207 <jose.ninob@uniagustiniana.edu.co>
2026-04-10 19:20:16 +0000

    commit clase 2

--- src/home/index.tsx
+++ src/home/index.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { Link } from 'react-router'
 
 interface Ranking {
   rank: number
@@ -7,9 +8,55 @@ interface Ranking {
   matchesPlayed: number
 }
 
+interface Estadistica {
+  position: number
+  name: string
+  value: number
+  appearances: number
+  contestantName: string
+  statName: string
+}
+
+
+type FiltroTipo = 'posiciones' | 'goleador' | 'asistencias' | 'amarillas' | 'atajadas'
+
+ const equiposMap: Record<string, string> = {
+  "América de Cali SA": "america-de-cali",
+  "CA Bucaramanga": "atletico-bucaramanga",
+  "AD Pasto" : "Pasto",
+  "Club Atlético Nacional SA": "atletico-nacional",
+  "Club Deportes Tolima SA": "deportes-tolima",
+  "Asociación Deportivo Cali": "deportivo-cali",
+  "Deportivo Independiente Medellín": "independiente-medellin",
+  "Club Independiente Santa Fe": "independiente-santa-fe",
+  "CD Popular Junior FC SA": "junior",
+  "Millonarios FC": "millonarios",
+  "Once Caldas SA": "once-caldas",
+
+  "Internacional de Bogotá": "internacional-bogota",
+  "Club Llaneros SA": "llaneros",
+  "Águilas Doradas": "aguilas-doradas",
+  "Fortaleza FC": "fortaleza",
+  "Alianza FC": "alianza",
+  "Jaguares de Córdoba FC": "jaguares",
+  "Cúcuta Deportivo FC": "cucuta",
+  "Boyacá Chicó FC": "boyaca-chico",
+  "Deportivo Pereira FC": "pereira"
+};
+
+
+
 function Home() {
+
   const [ranking, setRanking] = useState<Ranking[]>([])
   const [title, setTitle] = useState('')
+
+  const [filtro, setFiltro] = useState<FiltroTipo>('posiciones')
+  const [estadisticas, setEstadisticas] = useState<Estadistica[]>([])
+
+  const filtros: FiltroTipo[] = ['posiciones', 'goleador', 'asistencias', 'amarillas', 'atajadas']
+
+  const [busqueda, setBusqueda] = useState('')
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,7 +74,21 @@ function Home() {
     fetchData()
   }, [])
 
+ 
+ 
   return (
+  <>
+    <div className="filtros">
+        {filtros.map((onestat) => (
+          <button
+            key={onestat}
+            onClick={() => setFiltro(onestat)}
+            className={filtro === onestat ? 'activo' : ''}
+          >
+            {onestat}
+          </button>
+        ))}
+      </div>
     <div className="tabla-container">
       <h2>{title}</h2>
       <table className="tabla-posiciones">
@@ -43,7 +104,12 @@ function Home() {
           {ranking.map((equipo) => (
             <tr key={equipo.rank}>
               <td>{equipo.rank}</td>
-              <td>{equipo.contestantName}</td>
+              <td>
+              <Link to = {`/equipo/${equiposMap[equipo.contestantName] || "default"}`}>
+              {equipo.contestantName}
+              </Link>
+              </td>
+
               <td>{equipo.matchesPlayed}</td>
               <td>{equipo.points}</td>
             </tr>
@@ -51,6 +117,7 @@ function Home() {
         </tbody>
       </table>
     </div>
+    </>
   )
 }
 
